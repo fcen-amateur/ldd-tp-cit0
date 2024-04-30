bfd93341e521032206e40800964f629ee01c3f76
juanidan <142251397+juanidan@users.noreply.github.com>
2024-04-29 23:51:10 -0300

    Update tp0.py
    Grafico correspondiente y modificaciones para hacerlo ver mas lindo

--- plotters/tp0.py
+++ plotters/tp0.py
@@ -3,16 +3,19 @@ from gapminder import gapminder
 
 
 def plot():
+    paises = gapminder[gapminder["country"].isin(["Argentina", "Chile", "Bolivia",'Peru','Colombia','Venezuela'])]
     figura = (
         so.Plot(
-            gapminder[gapminder.continent == "Oceania"],
+            data=paises,
             x="year",
             y="lifeExp",
             color="country",
+            linestyle='country'
         )
-        .add(so.Line())
+        .layout(size=(8,6))
+        .add(so.Line(linewidth=3))
         .label(
-            title="Expectativa de vida en Oceanía",
+            title="Expectativa de vida en países por donde pasa la Cordillera de los Andes",
             x="Año",
             y="Expectativa de vida",
             color="País",
